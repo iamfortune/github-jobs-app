db0601bf7546ab930ffc744a44ff5188811bca2d
iamfortune <ikechifortune@gmail.com>
2020-07-27 00:45:11 +0100

    added badges and a card for the date

--- src/Job.js
+++ src/Job.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card } from 'react-bootstrap'
+import { Badge, Card } from 'react-bootstrap'
 
 export default function Job({ job }) {
     return (
@@ -7,10 +7,20 @@ export default function Job({ job }) {
         <Card.Body>
           <div className="d-flex justify-content-between">
             <div>
-                  <Card.Title>
-                  {job.title} - <span className="text-muted 
-                  font-weight-light">{job.company}</span>
-                </Card.Title>
+              <Card.Title>
+                {job.title} -{" "}
+                <span
+                  className="text-muted 
+                  font-weight-light"
+                >
+                  {job.company}
+                </span>
+              </Card.Title>
+              <Card.Subtitle className="text-muted mb-2">
+                {new Date(job.created_at.toLocaleDateString())}
+              </Card.Subtitle>
+              <Badge>{job.type}</Badge>
+              <Badge>{job.location}</Badge>
             </div>
           </div>
         </Card.Body>
