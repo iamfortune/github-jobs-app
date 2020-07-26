24160d7c3788905b2c8ad64d2f9f08087f586ab7
iamfortune <ikechifortune@gmail.com>
2020-07-26 19:25:04 +0100

    added a cancel token function and prevented cors by adding the cors anywhere bypss

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -1,11 +1,51 @@
-import { useReducer } from 'react'
+import { useReducer, useEffect } from 'react'
+import axios from 'axios'
+
+const ACTIONS = {
+    MAKE_REQUEST: 'make-request',
+    GET_DATA: 'get-data',
+    ERROR:'error'
+}
+
+const BASE_URL =
+  'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json'
+
+
+function reducer(state, action) {
+    
+    switch (action.type) {
+        case ACTIONS.MAKE_REQUEST: 
+            return { loading: true, jobs: [] }
+        case ACTIONS.GET_DATA: 
+            return { ...state, loading: false, jobs: action.payload.jobs }
+        case ACTIONS.ERROR:
+            return { ...state, loading: false, error: action.payload.error, jobs: [] }
+        default:
+            return state
+    }
+}
 
 export default function useFetchJobs(params, page) {
     const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true });
     
-    return {
-        jobs: [],
-        loading: false,
-        error: false
-    }
+    useEffect(() => {
+        const cancelToken = axios.CancelToken.source();
+        dispatch({ type: ACTIONS.MAKE_REQUEST })
+        axios.get(BASE_URL, {
+            cancelToken: cancelToken.token,
+            params: { markdwon: true, page: page, ...params }
+        }).then(res => {
+            dispatch({ type: ACTIONS.GET_DATA, payload: { jobs: res.data } })
+        }).catch(e => {
+           dispatch({ type: ACTIONS.ERROR, payload: { error: e } }) 
+        })
+
+        return () => {
+            
+        }
+
+    }, [params, page])
+
+
+    return state
 }
